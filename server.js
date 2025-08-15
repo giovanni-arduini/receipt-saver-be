0b25c8d4ccce91e435376f192b5b19987d840885
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-15 12:49:11 +0200

    ADD server w/ cors

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const URI =
+  "mongodb+srv://gionnibigudd:oYG15srUK4mtHdm6@receipt-saver.unuv8gx.mongodb.net/?retryWrites=true&w=majority&appName=receipt-saver";
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connessione a MongoDB
+
+async function connect() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ Connesso a MongoDB");
+  } catch (err) {
+    console.error("❌ Errore di connessione:", err);
+  }
+}
+
+// mongoose
+//   .connect(URI)
+//   .then(() => console.log("✅ Connesso a MongoDB"))
+//   .catch((err) => console.error("❌ Errore di connessione:", err));
+
+// Rotte
+const itemsRoute = require("./routes/items");
+app.use("/api/items", itemsRoute);
+
+// Avvio server
+connect();
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Server in ascolto sulla porta ${process.env.PORT}`);
+});
