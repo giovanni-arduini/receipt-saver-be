fc5dfd8daf6c524f94ab8e0c9a5fafd4f9f6101f
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-15 18:06:40 +0200

    ADD FOlder Schema

---
+++ models/Folder.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const FolderSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  id: { type: Number, required: true },
+  date: { type: String, required: true },
+  category: { type: String, reuquired: false },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Folder", FolderSchema);
