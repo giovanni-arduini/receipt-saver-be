edfea42be925179c558556b677dea4fc6a511515
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-15 12:55:15 +0200

    ADD counter for id for new items

--- models/File.js
+++ models/File.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const Counter = require("./Counter.js");
 
 const FileSchema = new mongoose.Schema({
   id: { type: Number, required: true },
   name: { type: String, required: true },
-  bounghtFrom: { type: String, required: false },
+  boughtFrom: { type: String, required: false },
   date: { type: String, required: true },
   category: { type: String, required: true },
   number: { type: String, required: false },
@@ -11,6 +12,18 @@ const FileSchema = new mongoose.Schema({
   special: { type: Boolean, required: true },
   folderId: { type: Number },
   createdAt: { type: Date, default: Date.now },
+});
+
+FileSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    const counter = await Counter.findOneAndUpdate(
+      { name: "fileId" },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+    this.id = counter.seq;
+  }
+  next();
 });
 
 module.exports = mongoose.model("File", FileSchema);
