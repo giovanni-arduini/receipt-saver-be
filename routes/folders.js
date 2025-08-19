cc81815d2ef01fec2eedf447e46b2aacf9806dfd
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-19 10:19:24 +0200

    ADD routes for folders

--- routes/folders.js
+++ routes/folders.js
@@ -2,11 +2,36 @@ const express = require("express");
 const router = express.Router();
 const Folder = require("../models/Folder");
 
-//Create new folder
+// Read folders
+router.get("/", async (req, res) => {
+  try {
+    const folders = await Folder.find();
+    res.json(folders);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
+// Show folder
+router.get("/:id", async (req, res) => {
+  try {
+    id = parseInt(req.params.id);
+    const folderToDelete = await Folder.findOneAndDelete(id);
+
+    if (!folderToDelete)
+      return res.status(404).json({ error: "Cartella non trovata" });
+
+    res.json({ message: "Cartella eliminata", file: folderToDelete });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: err.message });
+  }
+});
+
+//Create new folder
 router.post("/", async () => {
   try {
-    const lastFolder = await File.findOne().sort({ id: -1 });
+    const lastFolder = await Folder.findOne().sort({ id: -1 });
     const nextId = lastFolder ? lastFolder.id + 1 : 1;
 
     const newFolder = new Folder({
@@ -23,8 +48,22 @@ router.post("/", async () => {
   }
 });
 
-// Show folder
+// Update folder
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedFolder = await Folder.findOneAndUpdate(
+      { id: parseInt(req.params.id) },
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
 
-router.get("/", () => {});
+    if (!updatedFolder)
+      return res.status(404).json({ error: "File non trovato" });
+
+    res.json(updatedFolder);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
