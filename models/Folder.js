05f03649c7f73944fa3ca588c532d1da65e673b4
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-20 08:35:48 +0200

    FIX datt type for date

--- models/Folder.js
+++ models/Folder.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const FolderSchema = new mongoose.Schema({
   name: { type: String, required: true },
   id: { type: Number, required: true },
-  date: { type: String, required: true },
+  date: { type: Date, required: true },
   category: { type: String, reuquired: false },
   createdAt: { type: Date, default: Date.now },
 });
